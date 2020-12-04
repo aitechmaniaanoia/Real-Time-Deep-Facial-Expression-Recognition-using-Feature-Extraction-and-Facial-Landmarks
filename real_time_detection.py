a8710978442c0cd25f989e41717d32c058336cf7
zoe <wang1993x@gmail.com>
2020-12-03 22:05:14 -0800

    finish real time

--- real_time_detection.py
+++ real_time_detection.py
@@ -9,25 +9,56 @@ import numpy as np
 import cv2
 import argparse
 from keras.models import load_model
+# from tensorflow.keras.models import load_model
+from keras import backend as K
+from keras.optimizers import Adam
 
 import utils
+import config
+from model import *
+from dataset import *
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-m", "--model", type = str, help = "path to specific model checkpoint to load")
-args = vars(ap.parse_args())
+exp = 1
 
+if exp == 1:
+    model_path = 'output/experiment_1/.mdl_wts.hdf5'
+elif exp == 2:
+    model_path = 'output/experiment_2/.mdl_wts.hdf5'
+elif exp == 3:
+    model_path = 'output/experiment_3/.mdl_wts.hdf5'
+elif exp == 4:
+    model_path = 'output/experiment_4/.mdl_wts.hdf5'
 
-faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
 cap.set(3,640) # set Width
 cap.set(4,480) # set Height
 
-model = model = load_model(args["model"])
+# compile model
+if exp <= 2:
+    model = EmotionVGGNet.build(width = config.SIZE, height = config.SIZE, depth = config.NUM_CHANNEL,
+        classes = config.NUM_CLASSES)
+else:
+    model = EmotionVGGNet.build(width = config.SIZE, height = config.SIZE, depth = config.NUM_CHANNEL+1,
+        classes = config.NUM_CLASSES)
+    
+# opt = Adam(lr = 1e-3)
+# model.compile(loss = "categorical_crossentropy", optimizer = opt,
+#     metrics=['acc', utils.f1_m, utils.precision_m, utils.recall_m])
+
+model = load_model()
+
+landmark_detector = loadLBFmodel()
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+fontScale = 0.8
+fontColor = (255,0,0)
+lineType = 2
 
 while True:
     ret, img = cap.read()
-    img = cv2.flip(img, -1)
+    img = cv2.flip(img, 1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     # face alignment
@@ -41,17 +72,60 @@ while True:
     if faces:
         for (x,y,w,h) in faces:
             face = gray[y:y+h, x:x+w]/255
-            # predict emotion label
-            pred = model.predict(face)
             
-            pred = utils.label_num2txt(pred)
+            # pre-processing
+            face = cv2.resize(face, (config.SIZE, config.SIZE))
+            
+            norm_face = face/255
+            face_out = norm_face.reshape((config.SIZE, config.SIZE, config.NUM_CHANNEL))
+
+            if exp > 2:
+                # extract landmarks
+                bbox = np.array([[0,0,face.shape[0],face.shape[1]]])
+    
+                # Detect landmarks on "gray"
+                _, landmarks = landmark_detector.fit(face, bbox)
+                
+                landmarks = landmarks[0][0]
+                
+                # remove jaw index 0 to 17 
+                landmarks = landmarks[18:,:]
+                
+                idx1 = np.where(landmarks[:,0] > face.shape[1])[0]
+                idx2 = np.where(landmarks[:,1] > face.shape[0])[0]
+                idx = np.concatenate((idx1, idx2), axis=0)
+                if len(idx) > 0:
+                    # remove landmark
+                    landmarks = np.delete(landmarks, idx, axis=0)
+                
+                landmarks_layer = np.zeros(face.shape[:2])
+                landmarks_layer[landmarks[:,1].astype(int), landmarks[:,0].astype(int)] = 1
+                
+                face_out = np.zeros((config.SIZE, config.SIZE, config.NUM_CHANNEL+1))
+                face_out[:,:,:-1] = norm_face
+                face_out[:,:,-1] = landmarks_layer
+            
+            else:
+                face_out = norm_face
+                
+            # predict emotion label
+            pred = model.predict(face_out)
+            
+            label = utils.label_num2txt(pred)
             
             cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = img[y:y+h, x:x+w]
             
-            # TODO: show label in image
-            
+            # show label in image
+            bottomLeftCornerOfText = (x,y-10)
+        
+            cv2.putText(img,label, 
+                bottomLeftCornerOfText, 
+                font, 
+                fontScale,
+                fontColor,
+                lineType)
         
     cv2.imshow('video',img)
 
